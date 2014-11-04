8b6c10ad1c6368e893e7eba55e3e55e4d2b4410a
Marcel Erz <erz@yahoo-inc.com>
2014-11-04 12:56:29 -0800

    Add API-documentation and Coverage report

---
+++ docs/api.js
@@ -0,0 +1,33 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractListener",
+        "AbstractMessenger",
+        "AbstractReporter",
+        "DotReporter",
+        "DurationReporter",
+        "EventReporter",
+        "Hooks",
+        "JUnitReporter",
+        "Jenkins Sauce-Labs messenger",
+        "JenkinsSauceLabsReporter",
+        "JsonReporter",
+        "LineSummaryReporter",
+        "ListReporter",
+        "PlainReporter",
+        "PreceptorListener",
+        "PreceptorMessenger",
+        "PreceptorReporter",
+        "ReportContainer",
+        "ReportManager",
+        "SpecReporter",
+        "SummaryReporter",
+        "TapReporter",
+        "TeamCity listener",
+        "TeamCity messenger",
+        "TeamCityReporter"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
